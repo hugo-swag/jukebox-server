d54675b0bcebb173dc2e7b43f32575ed5ab0e2ff
Brooke Heck <b19heck@gmail.com>
2022-10-08 16:39:01 +0000

    refract server, add a few comments

--- public/client/app.js
+++ public/client/app.js
@@ -9,10 +9,18 @@ let localQueue = [];
 
 const eventPool = ['add', 'remove', 'bid'];
 
+// server sends queue as soon as a client joins
+socket.on('send-queue', (payload) => {
+  localQueue = payload;
+});
+
+// every socket is listening for events, so that when other clients update
+// the queue they get the up to date queue
 eventPool.forEach(singleEvent => socket.on(singleEvent, (payload) => {
   localQueue = payload;
   updateQueueList();
 }));
+
 
 songForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -27,7 +35,11 @@ function updateQueueList() {
   console.log(localQueue.songList);
   for(let song of localQueue.songList) {
     const li = document.createElement('li');
-    li.innerHTML = `${song.name} by ${song.artist}`;
+    li.innerHTML = `${song.name} by ${song.artist}: current bid at ${song.bid}`;
+    
+    const form = getBidForm(song);
+    li.appendChild(form);
+
     queueDiv.appendChild(li);
   }
 }
@@ -35,7 +47,6 @@ function updateQueueList() {
 function handleAddSong (name, artist, bid, songLength) {
   const song = {
     clientId: socket.id,
-    songId: Math.ceil(Math.random() * 10000),
     name: name,
     artist: artist,
     bid: bid,
@@ -45,9 +56,37 @@ function handleAddSong (name, artist, bid, songLength) {
   socket.emit('add', song);
 }
 
-function handleBid(socket, song, bid) {
+function handleBid(song, bid) {
   song.bid += bid;
   socket.emit('bid', song);
+}
+
+function getBidForm(song) {
+  // <label for="bid">Bid</label>
+  // <input type="number" id="bid" value="min" min="0" />
+
+  const label = document.createElement('label');
+  label.for = 'existingSongBid';
+
+  const input = document.createElement('input');
+  input.type = 'number';
+  input.id = 'existingSongBid';
+  input.min = '0';
+  input.value = '0';
+
+  const submit = document.createElement('input');
+  submit.type = 'submit';
+  submit.value = 'Bid';
+  submit.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleBid(song, 10);
+  });
+
+  const form = document.createElement('form');
+  form.appendChild(label);
+  form.appendChild(input);
+  form.appendChild(submit);
+  return form;
 }
 
 
