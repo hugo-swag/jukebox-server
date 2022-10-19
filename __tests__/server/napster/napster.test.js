e0be266fc27f8f2700b581e8103674bbcfca8ea3
Brooke Heck <b19heck@gmail.com>
2022-10-19 16:12:24 +0000

    add tests for napster api

--- __tests__/server/napster/napster.test.js
+++ __tests__/server/napster/napster.test.js
@@ -3,8 +3,22 @@
 const getSongData = require('./../../../server/napster/getSongData');
 
 describe('Should get up to 5 tracks from napster api', () => {
-  test('', async () => {
+  test('should return the top five search results', async () => {
     const response = await getSongData('heat waves', 'glass animals');
-    console.log(response);
+    
+    expect(response.length).toEqual(5);
+    expect(response[0].name).toBeDefined();
+    expect(response[0].artist).toBeDefined();
+    expect(response[0].uri).toBeDefined();
+  });
+
+  test('Should catch an error and return an empty array for no query strings', async () => {
+    const response = await getSongData();
+    expect(response).toEqual([]);
+  });
+
+  test('Should catch an error and return an empty array for empty strings for query strings', async () => {
+    const response = await getSongData('', '');
+    expect(response).toEqual([]);
   });
 });
--- server/napster/getSongData.js
+++ server/napster/getSongData.js
@@ -4,28 +4,25 @@ const axios = require('axios');
 const NAPSTER_API_KEY = process.env.NAPSTER_API_KEY;
 
 module.exports = async (songName, artist) => {
-  const queryWords = [...songName.split(' '), ...artist.split(' ')];
-  const queryString = queryWords.join('+');
-
-  const config = {
-    method: 'get',
-    url: `http://api.napster.com/v2.2/search?apikey=${NAPSTER_API_KEY}&query=${queryString}&type=track&per_type_limit=5`,
-  };
-
   try {
-    if(queryString) {
-      const response = await axios(config);
-      if(response.data.search.tracks) {
-        return response.data.search.data.tracks.map((track) => {
-          return new Song(track);
-        });
-      }
-    }  else {
-      return [];
-    }
+    const queryWords = [...songName.split(' '), ...artist.split(' ')];
+    const queryString = queryWords.join('+');
+
+    const config = {
+      method: 'get',
+      url: `http://api.napster.com/v2.2/search?apikey=${NAPSTER_API_KEY}&query=${queryString}&type=track&per_type_limit=5`,
+    };
+  
+    const response = await axios(config);
+    return response.data.search.data.tracks.map((track) => {
+      return new Song(track);
+    });
   } catch(e) {
     console.log(e.message);
+    return [];
   }
+
+  
 };
 
 class Song {
