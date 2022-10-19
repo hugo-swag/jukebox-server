533c94bbb04c4108860bd22ea4bbb8c80723831b
Brooke Heck <b19heck@gmail.com>
2022-10-19 15:50:10 +0000

    start napster tests

--- __tests__/server/napster/napster.test.js
+++ __tests__/server/napster/napster.test.js
@@ -3,5 +3,8 @@
 const getSongData = require('./../../../server/napster/getSongData');
 
 describe('Should get up to 5 tracks from napster api', () => {
-  
+  test('', async () => {
+    const response = await getSongData('heat waves', 'glass animals');
+    console.log(response);
+  });
 });
---
+++ __tests__/server/server.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('this should do something', () => {
+  test('true should be truthy', () => {
+    expect(true).toBeTruthy();
+  });
+});
